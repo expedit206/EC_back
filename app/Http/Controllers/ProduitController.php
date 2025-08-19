65318c2b9799190c83661ada158c60e152238782
Expedit206 <mciagnessi@gmail.com>
2025-08-19 11:52:37 +0100

    deployer

--- app/Http/Controllers/ProduitController.php
+++ app/Http/Controllers/ProduitController.php
@@ -167,11 +167,7 @@ class ProduitController extends Controller
             // Nouvelle vue, incrémenter views_count et enregistrer dans product_views
             $produit->counts()->updateOrCreate(
                 ['produit_id' => $produitId],
-                ['views_count' => \DB::
-                
-                
-                
-                ('views_count + 1')]
+                ['views_count' => \DB::raw('views_count + 1')]
             );
 
             ProductView::create([
