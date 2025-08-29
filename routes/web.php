f9adc7571c78607f85b1967e5d7ceca14fd6b64f
Expedit206 <mciagnessi@gmail.com>
2025-08-29 20:37:59 +0100

    auth token

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use Illuminate\Broadcasting\BroadcastController;
 
-Route::post('register', [UserController::class, 'register']);
-Route::post('api/v1/login', [UserController::class, 'login']);  
+// Route::post('register', [UserController::class, 'register']);
+// Route::post('api/v1/login', [UserController::class, 'login']);  
 
 // Route::post('/broadcasting/auth', [BroadcastController::class, 'authenticate']);
